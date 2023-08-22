6e57afffe725ec708a1ac525ce4be528f36b5820
l-blondy <laurent.blondy1@gmail.com>
2023-08-22 10:40:59 +0200

    remove upfetch export

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,3 @@
-import { createFetcher } from './createFetcher.js'
 export { createFetcher } from './createFetcher.js'
 export { FetcherOptions, DefaultOptions, RequestOptions } from './createFetcher.js'
 export { ResponseError, isResponseError } from './ResponseError.js'
-export let upfetch = createFetcher()
