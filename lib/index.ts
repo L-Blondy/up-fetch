eb1a3bfba7097ba7000b1a660609a29c222a2513
l-blondy <laurent.blondy1@gmail.com>
2024-01-20 15:08:18 +0100

    clear unused

--- lib/index.ts
+++
@@ -1,7 +0,0 @@
-export { createFetcher } from './createFetcher.js'
-export {
-   FetchOptions,
-   DefaultOptions,
-   RequestOptions,
-} from './createFetcher.js'
-export { ResponseError, isResponseError } from './ResponseError.js'
