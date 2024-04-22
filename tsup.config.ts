5db5aea967e52c84b99d7ba1d382c358b5b1ba7d
Laurent <laurent.blondy1@gmail.com>
2024-04-22 13:57:55 +0200

    add entrypoints

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-   entry: ['src/index.ts'],
+   // entry: ['src/index.ts'],
+   entry: {
+      index: 'src/index.ts',
+      'with-valibot': 'src/parsers/valibot.ts',
+      'with-zod': 'src/parsers/zod.ts',
+   },
    sourcemap: true,
    clean: true,
    dts: true,
