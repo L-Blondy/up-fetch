ced094a73cfcf0dabe7d306b8c32b4bed143a2e9
Laurent <laurent.blondy1@gmail.com>
2024-04-26 10:11:36 +0200

    up

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-   // entry: ['src/index.ts'],
    entry: {
       index: 'src/index.ts',
       'with-valibot': 'src/parsers/valibot.ts',
