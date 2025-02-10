6be14e13a13849d95161b4b4ba5dd87c3992f4b5
Laurent <laurent.blondy1@gmail.com>
2025-02-10 18:10:55 +0100

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { up } from './up'
 export type { FetcherOptions, DefaultOptions, ResolvedOptions } from './types'
-export { ResponseError, isResponseError } from './response-error'
+export { type ResponseError, isResponseError } from './response-error'
+export { type ValidationError, isValidationError } from './validation-error'
