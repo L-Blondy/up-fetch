22fd4b4e92cc559e2414f742b5e24a0e9a14f70c
Laurent Blondy <51233631+L-Blondy@users.noreply.github.com>
2025-02-22 14:34:25 +0100

    format

--- scripts/check-beta-version.js
+++ scripts/check-beta-version.js
@@ -1,5 +1,6 @@
 import pkg from '../package.json' with { type: "json" }
 const version = pkg.version
 
-if (!version.includes('beta'))
+if (!version.includes('beta')) {
    throw new Error('The package version is not valid for a beta release')
+}
