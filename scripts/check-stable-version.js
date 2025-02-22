341161702c24b79484b5a8e068ad8dcbe7344103
Laurent Blondy <51233631+L-Blondy@users.noreply.github.com>
2025-02-22 14:33:53 +0100

    format

--- scripts/check-stable-version.js
+++ scripts/check-stable-version.js
@@ -1,5 +1,6 @@
 import pkg from '../package.json' with { type: "json" }
 const version = pkg.version
 
-if (version.includes('beta') || version.includes('-'))
+if (version.includes('beta') || version.includes('-')) {
    throw new Error('The package version is not valid for a stable release')
+}
