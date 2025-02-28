6ef262e71ad7d2e56bd2862d15abe4813f83a36b
Laurent Blondy <51233631+L-Blondy@users.noreply.github.com>
2025-02-28 23:37:03 +0100

    fix: package.json path

--- scripts/check-stable-version.js
+++ scripts/check-stable-version.js
@@ -1,4 +1,4 @@
-import pkg from '../../package.json' with { type: 'json' }
+import pkg from '../package.json' with { type: 'json' }
 const version = pkg.version
 
 if (version.includes('beta') || version.includes('-')) {
