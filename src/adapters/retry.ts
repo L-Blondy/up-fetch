2f4fe90a79d1573c9aad2f8996a093e6e6057505
Laurent <laurent.blondy1@gmail.com>
2025-03-08 14:54:38 +0100

    improved retry syntax

--- src/adapters/retry.ts
+++ src/adapters/retry.ts
@@ -1,48 +1,63 @@
 import type { BaseFetchFn, MaybePromise, Prettify } from 'src/types'
 
 type RetryOptions = {
-   retryDelay?: (
-      attempt: number,
-      response: Response,
-      request: Request,
-   ) => MaybePromise<number>
-   retryTimes?: (response: Response, request: Request) => MaybePromise<number>
    retryWhen?: (response: Response, request: Request) => MaybePromise<boolean>
+   retryTimes?:
+      | number
+      | ((response: Response, request: Request) => MaybePromise<number>)
+   retryDelay?:
+      | number
+      | ((
+           attempt: number,
+           response: Response,
+           request: Request,
+        ) => MaybePromise<number>)
 }
-
-// TODO fix
-const request = new Request('https://a.b.c')
 
 export function withRetry<TFetchFn extends BaseFetchFn>(fetchFn: TFetchFn) {
    async function fetchWithRetry(
       input: Parameters<TFetchFn>[0],
-      options: Prettify<Parameters<TFetchFn>[1] & RetryOptions> = {} as any,
+      {
+         retryWhen = defaultRetryWhen,
+         retryTimes = 0,
+         retryDelay = 0,
+         ...options
+      }: Prettify<Parameters<TFetchFn>[1] & RetryOptions> = {} as any,
       ctx?: Parameters<TFetchFn>[2],
    ): Promise<Response> {
-      let [attempt, maxAttempt] = [0, 1]
+      const request = new Request(input, options as RequestInit)
+      let attempt = 1
+      let maxAttempt = 1
       let response: Response
-      while (attempt < maxAttempt) {
-         attempt++
+
+      do {
          response = await fetchFn(input, options, ctx)
-         const retryWhen = options.retryWhen ?? defaultRetryWhen
-         const shouldRetry = await retryWhen(response, request)
-         if (shouldRetry && maxAttempt === 1) {
-            maxAttempt += (await options.retryTimes?.(response, request)) ?? 0
+
+         if (await retryWhen(response, request)) {
+            if (maxAttempt === 1) {
+               maxAttempt +=
+                  typeof retryTimes === 'function'
+                     ? await retryTimes(response, request)
+                     : retryTimes
+            }
+            if (attempt < maxAttempt) {
+               await timeout(
+                  typeof retryDelay === 'function'
+                     ? await retryDelay(attempt, response, request)
+                     : retryDelay,
+               )
+            }
          }
-         if (shouldRetry && attempt < maxAttempt) {
-            const delay = await options.retryDelay?.(attempt, response, request)
-            delay && (await timeout(delay))
-         }
-      }
-      // @ts-expect-error the while loop will exec at least once
+         attempt++
+      } while (attempt <= maxAttempt)
+
       return response
    }
    return fetchWithRetry
 }
 
-function timeout(delay: number | undefined) {
-   return new Promise<void>((resolve) => setTimeout(() => resolve(), delay))
-}
+const timeout = (delay: number) =>
+   new Promise((resolve) => setTimeout(resolve, delay))
 
 // 408 - Request Timeout
 // 409 - Conflict
@@ -52,8 +67,6 @@ function timeout(delay: number | undefined) {
 // 502 - Bad Gateway
 // 503 - Service Unavailable
 // 504 - Gateway Timeout
-const retryStatusCodes = [408, 409, 425, 429, 500, 502, 503, 504]
-const retryMethods = ['GET', 'PUT', 'HEAD', 'DELETE', 'OPTIONS', 'TRACE']
 const defaultRetryWhen = (response: Response, request: Request) =>
-   retryStatusCodes.includes(response.status) &&
-   retryMethods.includes(request.method)
+   [408, 409, 425, 429, 500, 502, 503, 504].includes(response.status) &&
+   ['GET', 'PUT', 'HEAD', 'DELETE', 'OPTIONS', 'TRACE'].includes(request.method)
