7209d67a0f5d4d05e0d163276e11a9dba01bf85b
Laurent <laurent.blondy1@gmail.com>
2025-03-19 18:29:47 +0100

    fix node 18 test

--- src/tests/on-stream-request.spec.ts
+++ src/tests/on-stream-request.spec.ts
@@ -4,6 +4,8 @@ import { setupServer } from 'msw/node'
 import { up } from 'src/up'
 import { afterAll, afterEach, beforeAll, expect, test, vi } from 'vitest'
 
+const majorNodeVersion = Number(process.version.replace('v', '').split('.')[0])
+
 const baseUrl = 'http://a.b.c'
 const _1mb = 1024 * 1024
 
@@ -64,23 +66,26 @@ test('should call onStreamRequest for each chunk', async () => {
    })
 })
 
-test('should also chunk formData', async () => {
-   const upfetch = up(fetch, () => ({ baseUrl }))
-   let exec = 0
-   const image = fs.readFileSync(new URL('./2000@2x.png', import.meta.url))
-   const formData = new FormData()
-   formData.append(
-      'image',
-      new File([image], '2000@2x.png', { type: 'image/png' }),
-   )
+// File does not exist in node 18
+if (majorNodeVersion > 18) {
+   test('should also chunk formData', async () => {
+      const upfetch = up(fetch, () => ({ baseUrl }))
+      let exec = 0
+      const image = fs.readFileSync(new URL('./2000@2x.png', import.meta.url))
+      const formData = new FormData()
+      formData.append(
+         'image',
+         new File([image], '2000@2x.png', { type: 'image/png' }),
+      )
 
-   await upfetch('/large-blob', {
-      method: 'POST',
-      body: formData,
-      onStreamRequest(progress) {
-         ++exec
-         expect(progress.totalBytes).toBe(122640) // the size of the file
-      },
+      await upfetch('/large-blob', {
+         method: 'POST',
+         body: formData,
+         onStreamRequest(progress) {
+            ++exec
+            expect(progress.totalBytes).toBe(122640) // the size of the file
+         },
+      })
+      expect(exec).toBeGreaterThanOrEqual(3)
    })
-   expect(exec).toBeGreaterThanOrEqual(3)
-})
+}
