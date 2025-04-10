ed0a1c9c0bff8bc4eee1256e6430f6a509bb843a
Laurent <laurent.blondy1@gmail.com>
2025-04-10 13:46:45 +0200

    chore: test comments

--- src/tests/dist.spec.ts
+++ src/tests/dist.spec.ts
@@ -1,11 +1,28 @@
 import { up } from 'src/up'
-import { test } from 'vitest'
+import { expectTypeOf, test } from 'vitest'
+import { z } from 'zod'
 
 /**
- * @see https://github.com/L-Blondy/up-fetch/issues/43
+ * testing the build output with `declaration: true` in tsconfig.json
  *
- * due to `declaration: true` in tsconfig.json
+ * @see https://github.com/L-Blondy/up-fetch/issues/43
  */
-export const upfetch = up(fetch)
+export const distUpfetch = up(fetch, () => ({
+   parseResponse: Number,
+   baseUrl: 'https://a.b.c',
+}))
 
-test('placeholder', () => {})
+test('type inference should still work after the build', () => {
+   function noop(...args: any) {}
+
+   async function testCase() {
+      const d1 = await distUpfetch('')
+      expectTypeOf(d1).toEqualTypeOf<number>()
+      const d2 = await distUpfetch('', {
+         schema: z.number().transform(String),
+      })
+      expectTypeOf(d2).toEqualTypeOf<string>()
+   }
+
+   noop(testCase)
+})
