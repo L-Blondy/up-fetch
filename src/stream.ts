bdbc0cdeb967b425cd70921a83e00d140470dc18
Laurent <laurent.blondy1@gmail.com>
2025-07-24 14:34:34 +0200

    fix RN streaming & support safari/ios 14

--- src/stream.ts
+++ src/stream.ts
@@ -16,14 +16,13 @@ export async function toStreamable<R extends Request | Response>(
 ): Promise<R> {
    const isResponse = 'ok' in reqOrRes
    const isNotSupported = isWebkit && !isResponse
-   const body: (Response | Request)['body'] =
-      reqOrRes.body || (reqOrRes as any)._bodyInit
-   if (isNotSupported || !onStream || !body) return reqOrRes
+   // clone reqOrRes here to support IOS & Safari 14, otherwise support 15+
+   if (isNotSupported || !onStream || !getBody(reqOrRes, true)) return reqOrRes
    const contentLength = reqOrRes.headers.get('content-length')
    let totalBytes: number = +(contentLength || 0)
    // For the Request, when no "Content-Length" header is present, we read the total bytes from the body
    if (!isResponse && !contentLength) {
-      const reader = reqOrRes.clone().body!.getReader()
+      const reader = getBody(reqOrRes, true)!.getReader()
       while (true) {
          const { value, done } = await reader.read()
          if (done) break
@@ -39,7 +38,7 @@ export async function toStreamable<R extends Request | Response>(
 
    const stream = new ReadableStream({
       async start(controller) {
-         const reader = reqOrRes.body!.getReader()
+         const reader = getBody(reqOrRes)!.getReader()
          while (true) {
             const { value, done } = await reader.read()
             if (done) break
@@ -59,4 +58,13 @@ export async function toStreamable<R extends Request | Response>(
       ? (new Response(stream, reqOrRes) as R)
       : // @ts-expect-error outdated ts types
         (new Request(reqOrRes, { body: stream, duplex: 'half' }) as R)
+}
+
+const getBody = (
+   reqOrRes: Request | Response,
+   clone?: boolean,
+): (Request | Response)['body'] => {
+   const r: any = clone ? reqOrRes.clone() : reqOrRes
+   // r._bodyInit for React Native
+   return r.body || r._bodyInit
 }
