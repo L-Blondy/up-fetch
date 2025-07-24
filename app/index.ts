d576486aa2eeede54524439ccb49ef62ac159885
Laurent <laurent.blondy1@gmail.com>
2025-07-24 09:49:58 +0200

    add info

--- app/index.ts
+++ app/index.ts
@@ -6,6 +6,26 @@ const isWebkit =
    /AppleWebKit/i.test(window.navigator.userAgent) &&
    !/Chrome/i.test(window.navigator.userAgent)
 
+function checkRequest() {
+   let duplexUsed = false
+   let contentType: string | null = null
+   try {
+      const req = new Request('https://a.b.c', {
+         method: 'POST',
+         body: new ReadableStream(),
+         // @ts-expect-error outdated types
+         get duplex() {
+            duplexUsed = true
+            return 'half'
+         },
+      })
+      contentType = req.headers.get('Content-Type')
+   } catch (_) {
+      return { duplexUsed, duplexThrows: true, contentType }
+   }
+   return { duplexUsed, duplexThrows: false, contentType }
+}
+
 async function request() {
    return up(fetch)('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
@@ -27,16 +47,17 @@ async function response() {
 }
 
 async function main() {
+   const duplex = checkRequest()
    const requestOutcome = await request()
    const responseOutcome = await response()
 
    document.getElementById('is-webkit')!.append(String(isWebkit))
    document
       .getElementById('request-streaming')!
-      .append(JSON.stringify(requestOutcome, null, 3))
+      .append(JSON.stringify({ ...requestOutcome, duplex }, null, 3))
    document
       .getElementById('response-streaming')!
-      .append(JSON.stringify(responseOutcome, null, 3))
+      .append(JSON.stringify({ ...responseOutcome, duplex }, null, 3))
 }
 
 main()
